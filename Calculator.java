63f8b0c7b5539c567ad8aee82129d83c56113134
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-06-24 08:24:30 -0400

    Add files via upload
    Uploaded updated version of standard deviation calculator to allow the user to input the number of terms s/he is testing.  I also included the class I created to store all of my math functions.  I plan to add more calculations for a more complete statistical analysis.

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,8 @@
 /////////////////////////////////////////////////////////////////////////////////////////////
-//Created By: Jacob Cassady
-//Date first created: 03/24/2016
-//Project: CECS 220-01; Assignment 5
-//Description: Write a program that computes and prints the mean and standard deviation
-//of a list of integers x1 through xn.  Assume that there will be no more
-//than 50 input values.  Compute both the mean and standard deviation as
-//floating point values.
+//		Created By: Jacob Cassady
+//		Date first created: 03/24/2016
+//		Date last updated: 06/24/2016
+//		Class: CECS 220-01 (Object Oriented Program Design with Java)
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 import java.util.*;
@@ -15,55 +12,14 @@ import java.awt.event.*;
 import java.text.DecimalFormat;
 
 public class Calculator extends JPanel{
-	private JPanel inputPanel, outputPanel, buttonPanel;
-	private JLabel inputLabel, standardDeviationLabel, meanLabel;
-	private JTextArea inputTextArea;
+	private JPanel inputPanel, NPanel, outputPanel, buttonPanel;
+	private JLabel inputLabel,standardDeviationLabel, meanLabel;
+	private JTextArea inputTextArea, inputNArea;
 	private JButton compute;
-	private int arrayLength;
+	private int n;
 	private float standardDeviation;
 	private DecimalFormat dfm;
 	private int[] dataSet;
-
-	public Calculator(){
-	standardDeviation = 0;
-	Random randomNumberGenerator = new Random();
-	dfm = new DecimalFormat("0.00");
-	arrayLength = randomNumberGenerator.nextInt(50);
-	dataSet = new int[arrayLength];
-	
-	inputPanel = new JPanel();
-	inputLabel = new JLabel("Input " + arrayLength + " integers:");
-	inputTextArea = new JTextArea("Enter a set of " + arrayLength + " integers, each separated by a space and I will return the standard deviation for the data set.",3,75);
-	inputTextArea.setLineWrap(true);
-	inputTextArea.setWrapStyleWord(true);
-	inputPanel.setLayout(new BorderLayout());
-	inputPanel.add(inputLabel, BorderLayout.NORTH);
-	inputPanel.add(inputTextArea, BorderLayout.CENTER);
-	inputPanel.setPreferredSize(new Dimension(300,75));
-	
-	buttonPanel = new JPanel();
-	compute = new JButton("Compute");
-	compute.addActionListener(new ButtonListener());
-	buttonPanel.setLayout(new BorderLayout());
-	buttonPanel.add(compute,BorderLayout.CENTER);
-	buttonPanel.setPreferredSize(new Dimension(300,25));
-	
-	outputPanel = new JPanel();
-	standardDeviationLabel = new JLabel("The standard deviation is: " + dfm.format(standardDeviation));
-	meanLabel = new JLabel("The mean is: " + dfm.format(0));
-	
-	//outputPanel.setLayout(new BorderLayout());
-	outputPanel.add(standardDeviationLabel);
-	outputPanel.add(meanLabel);
-	outputPanel.setPreferredSize(new Dimension(300,50));
-	
-	
-	
-	add(inputPanel);
-	add(buttonPanel);
-	add(outputPanel);
-	setPreferredSize(new Dimension(310,160));
-	}
 	
 	public static void main(String args[]){
 		JFrame frame = new JFrame("Standard Deviation");
@@ -72,28 +28,81 @@ public class Calculator extends JPanel{
 		frame.getContentPane().add(new Calculator());
 		frame.pack();
 		frame.setVisible(true);
-	}	
+	}
 
+	public Calculator(){
+		standardDeviation = 0;
+		dfm = new DecimalFormat("0.00");
+		
+		inputPanel = new JPanel();
+		NPanel = new JPanel();
+		inputLabel = new JLabel("Input size of data set:" );
+		inputNArea = new JTextArea(1,2);
+		NPanel.add(inputLabel);
+		NPanel.add(inputNArea);
+		inputTextArea = new JTextArea("Enter a set of n integers, each separated by a space and I will return the standard deviation for the data set.",3,75);
+		inputTextArea.setLineWrap(true);
+		inputTextArea.setWrapStyleWord(true);
+		inputPanel.setLayout(new BorderLayout());
+		inputPanel.add(NPanel, BorderLayout.NORTH);
+		inputPanel.add(inputTextArea, BorderLayout.CENTER);
+		inputPanel.setPreferredSize(new Dimension(300,75));
+		
+		buttonPanel = new JPanel();
+		compute = new JButton("Compute");
+		compute.addActionListener(new ButtonListener());
+		buttonPanel.setLayout(new BorderLayout());
+		buttonPanel.add(compute,BorderLayout.CENTER);
+		buttonPanel.setPreferredSize(new Dimension(300,25));
+		
+		outputPanel = new JPanel();
+		standardDeviationLabel = new JLabel("The standard deviation is: " + dfm.format(standardDeviation));
+		meanLabel = new JLabel("The mean is: " + dfm.format(0));
+		
+		outputPanel.add(standardDeviationLabel);
+		outputPanel.add(meanLabel);
+		outputPanel.setPreferredSize(new Dimension(300,50));
+		
+		
+		
+		add(inputPanel);
+		add(buttonPanel);
+		add(outputPanel);
+		setPreferredSize(new Dimension(310,160));
+	}
+	
 	private class ButtonListener implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent event) {
-		String text = "";
-		int count=0;
-		
-		text = inputTextArea.getText();
-		
-		Scanner scan = new Scanner(text);
-		while(scan.hasNext() && (count < arrayLength)){
-		dataSet[count] = scan.nextInt();
-		count++;
+			String text = "";
+			int count=0;
+			
+			text = inputNArea.getText();
+			Scanner scan = new Scanner(text);
+			
+			if(scan.hasNext()){ //retrieve data from inputNArea to find number of terms
+				n = scan.nextInt();
+				dataSet = new int[n];
+			} else {
+				
+			}
+			scan.close();
+
+			text = inputTextArea.getText();
+			scan = new Scanner(text);
+			
+			while(scan.hasNext() && (count < n)){ //Retrieve data input and fill in array dataSet
+				dataSet[count] = scan.nextInt();
+				count++;
+			}
+			
+			standardDeviation = JMath.standardDeviation(dataSet);
+
+			standardDeviationLabel.setText("The standard deviation is: " + dfm.format(standardDeviation));
+			meanLabel.setText("The mean is: " + dfm.format(JMath.mean(dataSet)));
+			
+			scan.close();
 		}
 		
-		standardDeviation = JMath.standardDeviation(dataSet);
-		
-		standardDeviationLabel.setText("The standard deviation is: " + dfm.format(standardDeviation));
-		meanLabel.setText("The mean is: " + dfm.format(JMath.mean(dataSet)));
-		
-		scan.close();
-		}
 	}
 }
---
+++ JMath.java
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//		Created By: Jacob Cassady
+//		Date first created: 03/24/2016
+//		Date last updated: 06/24/2016
+//		Class: CECS 220-01 (Object Oriented Program Design with Java)
+//		Description: Static class used to hold math functions used in Project85
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+public class JMath {
+	private JMath(){}
+	
+	public static float standardDeviation(int[] dataSet){
+		float mean = 0;
+		Float result = new Float(0);
+		mean = mean(dataSet);
+		
+		for (int dataSet_i=0;dataSet_i<dataSet.length;dataSet_i++){
+			result += square(dataSet[dataSet_i] - mean);
+		}
+		
+		result /= dataSet.length;
+		
+		result = (float) Math.sqrt(result);
+		
+		return result;
+	}
+	
+	
+	public static float mean(int[] dataSet){
+		float result = 0;
+		int sum = 0;
+		
+		for(int dataSet_i=0; dataSet_i<dataSet.length; dataSet_i++){
+			sum += dataSet[dataSet_i];
+		}
+		
+		result = ((float)sum) / ((float) dataSet.length);
+		
+		return result;
+	}
+	
+	public static float square(float num){
+		return num*num;
+	}
+}
